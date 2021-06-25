4679d83fc1c9547367a2ebdb901586c6c60c9875
Bunks <busk_1@yahoo.com>
2021-06-24 23:08:01 -0400

    major refactor from material ui to bulma

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,51 +1,34 @@
+import React from "react";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import { useForm } from "react-hook-form";
+
 import React from 'react';
-import Grid from '@material-ui/core/Grid';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Typography from "@material-ui/core/Typography";
-import Paper from "@material-ui/core/Paper";
-import Divider from "@material-ui/core/Divider";
-import InputField from '../components/InputField'
-import TextButton from '../components/TextButton'
 
-const useStyles=makeStyles(theme => ({
-    mainContainer: {
-        marginLeft: theme.spacing(35),
-        marginTop: theme.spacing(13),
-        marginBottom: "5em",
-        width: "70%",
-        justifyContent: "space-evenly",
-        [theme.breakpoints.down("md")]:{
-            width: "80%"
-        },
-        [theme.breakpoints.down("sm")]:{
-            width: "70%"
-        },
-        [theme.breakpoints.down("xs")]:{
-            spacing: theme.spacing(2),
-            marginLeft: "3.5em"
-        }
-    },
-}))
+const Login = () => {
 
-export default function Instructor(){
-    const classes = useStyles();
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+    control,
+  } = useForm();
 
-    return(
-        <Grid container className={classes.mainContainer}>
-            <Grid item align="center" style={{marginBottom: "1em"}} direction="column">
-                <Typography variant="h2" color="primary">Fist to Five</Typography>
-                <Typography variant="h5" color="secondary">Login</Typography>
-                <Divider />
-            </Grid>
-            <Grid item align="center" style={{marginBottom: "1em"}} direction="column">
-                <InputField label="User Name"/>
-            </Grid>
-            <Grid item align="center" style={{marginBottom: "1em"}} direction="column">
-                <InputField label="Password"/>
-            </Grid>
-            <Grid item align="center" style={{marginBottom: "1em"}} direction="column">
-                <TextButton text="Let's Go" link=""/>
-            </Grid>
-        </Grid>
-    )
-}
+  const onSubmit = async (data) => {
+    try {
+      console.log("Hitting submit.");
+      console.log(data);
+      reset();
+    } catch (error) {
+      console.error("ERROR: ", error);
+    }
+  };
+
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default Login;
