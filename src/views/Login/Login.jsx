47c813a5722441d1504c494f14d1a91f1b0d79bc
Bunks <busk_1@yahoo.com>
2021-06-24 23:48:27 -0400

    added very basic form

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -3,10 +3,9 @@ import "./Login.scss";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 import { useForm } from "react-hook-form";
 
-import React from 'react';
+import React from "react";
 
 const Login = () => {
-
   const {
     register,
     formState: { errors },
@@ -26,7 +25,29 @@ const Login = () => {
 
   return (
     <div className="loginContainer">
-      
+      <div className="loginWrapper">
+        <h3>Login</h3>
+        <form
+          className="loginForm"
+          onSubmit={handleSubmit(onSubmit)}
+          noValidate
+        >
+          <div className="field">
+            <label className="label">Username</label>
+            <div className="control">
+              <input type="text" />
+            </div>
+          </div>
+          <div className="field">
+            <label className="label">Password</label>
+            <div className="control">
+              <input type="password" />
+            </div>
+          </div>
+          <button>Login</button>
+          <button>Create Account</button>
+        </form>
+      </div>
     </div>
   );
 };
