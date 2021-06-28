10e0a17be11e38727832af9dc1295d8742348ce1
Bunks <busk_1@yahoo.com>
2021-06-27 23:11:28 -0400

    made a new activity form to accept new data

--- src/components/NewActivityForm.js
+++ src/components/NewActivityForm.js
@@ -1,42 +1,9 @@
+import React from "react";
+import api from "../api/index.js";
 import { useForm } from "react-hook-form";
-import api from "../api/index";
-import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
-export default function ActivityForm(props) {
-  const { activityDescription, activityName, filePath, status, weekday, id } =
-    props.activityInfo.info;
-
-  const [formName, setFormName] = useState("");
-  const [formValues, setFormValues] = useState({
-    id: "",
-    activityDescription: "",
-    activityName: "",
-    filePath: "",
-    status: "",
-    weekday: "",
-  });
-
-  let history = useHistory();
-
-  // const onChange = (event) => {
-  //   const { name, value } = event.target;
-  //   console.log("hi this is onchange")
-  //   setFormValues({...formValues, [name]: value})
-  // };
-
-  useEffect(() => {
-    setFormValues({
-      id,
-      activityDescription,
-      activityName,
-      filePath,
-      status,
-      weekday,
-    });
-  }, [id, activityDescription, activityName, filePath, status, weekday]);
-
-  console.log(props);
+const NewActivityForm = () => {
   const {
     register,
     formState: { errors },
@@ -44,15 +11,17 @@ export default function ActivityForm(props) {
     reset,
   } = useForm();
 
-  const updatingActivity = async (data) => {
-    const newData = { ...data, id };
+  let history = useHistory();
+
+  const createActivity = async (data) => {
+    console.log(data);
     try {
-      console.log("This is the activity data: ", newData);
-      api.updateActivity(id, newData).then((data) => {
-        console.log("This is update axios", data);
+      console.log("This is the activity data: ", data);
+      api.createActivity(data).then((data) => {
+        console.log("This is post axios", data);
       });
       reset();
-      history.push("/instructor")
+      history.push("/instructor");
     } catch (error) {
       console.error("ERROR: ", error);
     }
@@ -66,18 +35,16 @@ export default function ActivityForm(props) {
       <div class="message-body">
         <form
           className="instructorForm"
-          onSubmit={handleSubmit(updatingActivity)}
+          onSubmit={handleSubmit(createActivity)}
           noValidate
         >
           <div className="field">
             <label className="label">Activity Name:</label>
             <div className="control">
               <input
-                defaultValue={formValues.activityName}
                 type="text"
                 {...register("activityName", { required: true })}
                 className="input is-normal is-primary"
-                // onChange={onChange}
               />
               {errors.activityName && (
                 <span className="errorMessage">
@@ -90,7 +57,6 @@ export default function ActivityForm(props) {
             <label className="label">File Path:</label>
             <div className="control">
               <input
-                value={filePath}
                 type="text"
                 {...register("filePath", { required: true })}
                 className="input is-normal is-primary"
@@ -106,7 +72,6 @@ export default function ActivityForm(props) {
             <label className="label">Activity Description:</label>
             <div className="control">
               <input
-                value={activityDescription}
                 type="text"
                 {...register("activityDescription", { required: true })}
                 className="input is-normal is-primary"
@@ -122,7 +87,6 @@ export default function ActivityForm(props) {
             <label className="label">Weekday:</label>
             <div className="control">
               <input
-                value={weekday}
                 type="text"
                 {...register("weekday", { required: true })}
                 className="input is-normal is-primary"
@@ -138,7 +102,6 @@ export default function ActivityForm(props) {
             <label className="label">Status:</label>
             <div className="control">
               <input
-                value={status}
                 type="text"
                 {...register("status", { required: true })}
                 className="input is-normal is-primary"
@@ -151,10 +114,12 @@ export default function ActivityForm(props) {
             </div>
           </div>
           <button class="button is-success" type="submit">
-            {activityName ? "Update Activity" : "Create Activity"}
+            Create Activity
           </button>
         </form>
       </div>
     </article>
   );
-}
+};
+
+export default NewActivityForm;
